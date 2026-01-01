5c921ca2a9721ce25846b126d0a9ba674f32d5aa
xkonni <xkonni@posteo.de>
2026-01-01 12:46:24 +0100

    sway: fix and improve lockscreen

--- dot_config/sway/bin/executable_start_swayidle.sh
+++ dot_config/sway/bin/executable_start_swayidle.sh
@@ -12,10 +12,10 @@ PWR_CMD="$HOME/.config/sway/bin/sway_power"
 #  120 min    suspend
 pkill -x swayidle
 swayidle -w \
-    timeout 540 "echo $TERM_CMD bash -c 'dunstify -r 123 -u critical -t 300 display\ will\ turn\ off\ soon'" \
-    resume "echo $LOCK_CMD on" \
-    timeout 600 "echo $LOCK_CMD off" \
-    timeout 900 "echo $LOCK_CMD lockonly" \
-    timeout 3600 "echo $PWR_CMD suspend-battery" \
-    timeout 7200 "echo $PWR_CMD suspend" \
-    before-sleep "echo $LOCK_CMD lock"
+    timeout 540 "$TERM_CMD bash -c 'dunstify -r 123 -u critical -t 300 display\ will\ turn\ off\ soon'" \
+    resume "$LOCK_CMD on" \
+    timeout 600 "$LOCK_CMD off" \
+    # timeout 900 "$LOCK_CMD lockonly" \
+    # timeout 3600 "$PWR_CMD suspend-battery" \
+    # timeout 7200 "$PWR_CMD suspend" \
+    # before-sleep "$LOCK_CMD lock"
