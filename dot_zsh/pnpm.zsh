b6bf4e8e49d9a082ba91a35e7825d89b7c86448f
Konstantin Koslowski <konstantin.koslowski@sap.com>
2026-05-06 11:57:10 +0200

    use ifeq darwin

--- dot_zsh/pnpm.zsh
+++ dot_zsh/pnpm.zsh
@@ -1,6 +1,6 @@
 
 # pnpm
-export PNPM_HOME="/home/konni/.local/share/pnpm"
+export PNPM_HOME="$HOME/.local/share/pnpm"
 case ":$PATH:" in
   *":$PNPM_HOME:"*) ;;
   *) export PATH="$PNPM_HOME:$PATH" ;;
