db9aa333bbd3e600033d95fcc4c9a5005fd7971a
Konstantin Koslowski <konstantin.koslowski@sap.com>
2026-06-02 08:37:20 +0200

    zsh: try to improve startup times by caching
    Signed-off-by: Konstantin Koslowski <konstantin.koslowski@sap.com>

--- dot_zsh/zoxide.zsh
+++ dot_zsh/zoxide.zsh
@@ -1,5 +1,8 @@
-if type -f zoxide &> /dev/null
-then
-    eval "$(zoxide init zsh)"
+if type -f zoxide &> /dev/null; then
+  _zoxide_cache="${XDG_CACHE_HOME:-$HOME/.cache}/zoxide_init.zsh"
+  if [[ ! -f "$_zoxide_cache" ]]; then
+    zoxide init zsh >! "$_zoxide_cache" 2>/dev/null
+  fi
+  [[ -f "$_zoxide_cache" ]] && source "$_zoxide_cache"
+  unset _zoxide_cache
 fi
-
---
+++ run_after_clear-zsh-completion-cache.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+rm -f "${XDG_CACHE_HOME:-$HOME/.cache}/cld_completion.zsh"
+rm -f "${XDG_CACHE_HOME:-$HOME/.cache}/helm_completion.zsh"
+rm -f "${XDG_CACHE_HOME:-$HOME/.cache}/zoxide_init.zsh"
